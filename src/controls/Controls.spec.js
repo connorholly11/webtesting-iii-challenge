9aeab44fbbd7610e8f16142ae55a2ec3a0ace6ad
Connor Holly <connorholly11@gmail.com>
2019-09-16 14:43:27 -0600

    4 tests done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+import Controls from './Controls'
+import React from 'react'
+import {render, fireEvent } from '@testing-library/react';
+
+
+
+test('defaults to locked and unlocked', () => {
+    const toggleLocked = jest.fn()
+    const toggleClosed = jest.fn()
+    const locked = false
+    const closed = false
+
+    render (<Controls 
+        locked={locked} 
+        closed={closed} 
+        toggleLocked={toggleLocked} 
+        toggleClosed = {toggleClosed}
+        />)
+    
+})
+
+// const lockedButton = getByTestId(/lockedbutton/i);
+//     const openButton = getByTestId(/openbutton/i);
+
+//     fireEvent.click(lockedButton.disabled);
+
+//     expect(lockedButton).to
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,30 @@
 // Test away
+
+import React from 'react';
+import {render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+import Controls from '../controls/Controls'
+import Display from '../display/Display'
+
+test('is dashboard rendering properly', () => {
+    render(<Dashboard/>)
+})
+
+test('is controls rendering properly', () => {
+    render(<Controls/>)
+})
+
+test('is display rendering properly', () => {
+    render(<Display/>)
+})
+
+// test('is default unlocked and open', () => {
+//     const {getByText} = render(<Dashboard/>)
+
+//     getByText(/locked/i)
+
+// })
+
+
+
+
