12ca61068a252a868f15ceddba837219d22c4db5
Connor Holly <connorholly11@gmail.com>
2019-09-17 13:20:13 -0600

    display done, dashboard done, controls almost done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,9 @@
 // Test away!
 import Controls from './Controls'
+import Dashboard from '../dashboard/Dashboard';
 import React from 'react'
-import {render, fireEvent } from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
+import { get } from 'https';
 
 
 
@@ -22,14 +24,50 @@ test('defaults to locked and unlocked', () => {
     expect(closed).toBe(false)
 })
 
-test('test the colors of the buttons', () => {
-    const display = render(<Controls closed={false} locked={false}/>)
-    display.getByText('Closed')
-    display.getByText('Unlocked')
+test('text changes to the state the door will be in if its clicked', () => {
+    const {getByText} = render(<Dashboard />)
+    const CloseButton = getByText(/close gate/i)
+    const LockButton = getByText(/lock gate/i)
 
-    expect(display.getByText('Closed').className).toContain('red-led')
-    expect(display.getByText('Unlocked').className).toContain('green-led')
+
+    getByText(/close gate/i);
+
+    fireEvent.click(CloseButton);
+
+    getByText(/open gate/i)
+    getByText(/lock gate/i)
+
+    fireEvent.click(LockButton);
+
+    const UnlockButton = getByText(/unlock gate/i)
+
+    getByText(/unlock gate/i)
+
+    fireEvent.click(UnlockButton)
+
+    const OpenButton = getByText(/open gate/i)
+
+    getByText(/open gate/i)
+    getByText(/lock gate/i)
+
+    fireEvent.click(OpenButton);
+
+    getByText(/close gate/i)
 })
+
+// test('test the colors of the buttons', () => {
+//     const display = render(<Controls closed={false} locked={false}/>)
+//     display.getByText(/Closed/i)
+//     display.getByText(/unlocked/i)
+
+//     expect(display.getByText('Closed').className).toContain('red-led')
+//     expect(display.getByText('Unlocked').className).toContain('green-led')
+// })
+
+// test('displays closed when closed prop is true', () => {
+//     const display = render(<Controls />)
+//     display.getByText
+// })
 
 // const lockedButton = getByTestId(/lockedbutton/i);
 //     const openButton = getByTestId(/openbutton/i);
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,44 @@
+// render and then pass in the prop your looking for
+//get the getByText with the word your looking for 
+//then expect(^).toHaveClass('red-led')
+
 // Test away!
+
+import React from 'react';
+import {render, getByText} from '@testing-library/react';
+import Display from './Display';
+import '@testing-library/jest-dom/extend-expect'
+
+test('locked and closed are set to false', () => {
+    render(<Display/>)
+    expect(Display.defaultProps.closed).toBe(false);
+    expect(Display.defaultProps.locked).toBe(false);
+})
+
+
+test('display if gate is locked', () => {
+    const {getByText} = render (<Display locked={false}/>)
+    expect(getByText('Unlocked'))
+})
+
+test ('display if gate is closed',() => {
+    const {getByText} = render(<Display closed={true}/>)
+    expect(getByText('Closed'))
+})
+
+test('display if gate is open', () => {
+    const {getByText} = render(<Display open={false} />)
+    expect(getByText('Open'))
+})
+
+test('red-led class when locked or closed', ()=> {
+    const {getByText} = render(<Display locked={true} />)
+    const locked = getByText('Locked')
+    expect(locked).toHaveClass('red-led')
+})
+
+test('green-led when locked or closed', () => {
+    const {getByText} = render(<Display locked={false}/>)
+    const locked = getByText('Open')
+    expect(locked).toHaveClass('green-led')
+})
